869e95c5a7e3adce5b8f7a85e5f9239f0b34f027
ivanlikhvatov <61515523+ivanlikhvatov@users.noreply.github.com>
2020-12-05 02:07:35 +0400

    sending message, login with google, db with user and message and other

---
+++ src/main/java/com/example/socialNetwork/controller/MainController.java
@@ -0,0 +1,42 @@
+package com.example.socialNetwork.controller;
+
+
+import com.example.socialNetwork.domain.User;
+import com.example.socialNetwork.repo.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    private final MessageRepo messageRepo;
+
+    @Value("${spring.profiles.active}")
+    private String profile;
+
+    @Autowired
+    public MainController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+
+    @GetMapping
+    public String main(Model model, @AuthenticationPrincipal User user){
+        Map<Object, Object> data = new HashMap<>();
+        data.put("profile", user);
+        data.put("messages", messageRepo.findAll());
+
+        model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode", "dev".equals(profile));
+
+        return "index";
+    }
+}
